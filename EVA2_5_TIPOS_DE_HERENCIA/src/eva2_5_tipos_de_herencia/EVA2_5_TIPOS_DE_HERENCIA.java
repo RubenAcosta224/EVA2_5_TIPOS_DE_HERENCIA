7074fa9d21a413ae3e04810ca8b646c47c5c8b59
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-03-13 22:42:01 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_DE_HERENCIA/src/eva2_5_tipos_de_herencia/EVA2_5_TIPOS_DE_HERENCIA.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_5_tipos_de_herencia;
+
+/**
+ *
+ * @author dell
+ */
+public class EVA2_5_TIPOS_DE_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        }
+        // Existen 2 tipos de herencia: múltiple y simole     
+        // Java solo permite simple     
+        // Problemas : coliciones de elementos
+        // Se puede simular herencia con interfaces  
+    }
+        class SuperClase{ 
+        
+    }
+
+    class OtraSuperClase{
+   //No se hereda de mas de 2 superclases   
+        
+    }
+
+    class BaseClase extends SuperClase{
+    //Funciona unicamente para una clase
+        
+    }
+}
